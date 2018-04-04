0ec99643bbb08c03e61abde55609d61dc340fbb8
sping <sping@stu.zzu.edu.cn>
2018-04-04 15:23:40 +0800

    change format

--- HashTable.c
+++ HashTable.c
@@ -13,150 +13,144 @@
 /*
  *
  * */
- 
+
 int preProc(FILE *stream, char *res);
- 
- 
+
 unsigned int hash(char* str) {
-  unsigned int hash = 0;
-  int i;
+	unsigned int hash = 0;
+	int i;
 
-  for (i = 0; *str; i++) {
-    if ((i & 1) == 0) {
-      hash ^= ((hash << 7) ^ (*str++) ^ (hash >> 3));
-    } else {
-      hash ^= (~((hash << 11) ^ (*str++) ^ (hash >> 5)));
-    }
-  }
+	for (i = 0; *str; i++) {
+		if ((i & 1) == 0) {
+			hash ^= ((hash << 7) ^ (*str++) ^ (hash >> 3));
+		} else {
+			hash ^= (~((hash << 11) ^ (*str++) ^ (hash >> 5)));
+		}
+	}
 
-  return (hash & 0x7FFFFFFF) % 80;
+	return (hash & 0x7FFFFFFF) % 80;
 }
 
 Status InsertHash(HashTable *H, ElemType elem) {
 
-  unsigned int p;
-  if (SearchHash(*H, elem.key, &p))
-    return DUPLICATE;
-  else {
-    H->elem[p] = elem;
-    ++H->count;
-    return OK;
-  }
+	unsigned int p;
+	if (SearchHash(*H, elem.key, &p))
+		return DUPLICATE;
+	else {
+		H->elem[p] = elem;
+		++H->count;
+		return OK;
+	}
 }
 
 Status SearchHash(HashTable H, char *key, unsigned int *pp) {
-  *pp = hash(key);
-  int si = 1;
-  while (H.elem[*pp].key[0] != 0 && strcmp(H.elem[*pp].key, key) != 0) {
-    collision(si, pp);
-    si ++;
-  }
-  if (!strcmp(key, H.elem[*pp].key))
-    return SUCCESS;
-  else
-    return UNSUCCESS;
+	*pp = hash(key);
+	int si = 1;
+	while (H.elem[*pp].key[0] != 0 && strcmp(H.elem[*pp].key, key) != 0) {
+		collision(si, pp);
+		si++;
+	}
+	if (!strcmp(key, H.elem[*pp].key))
+		return SUCCESS;
+	else
+		return UNSUCCESS;
 }
 
 void collision(int si, unsigned int *pp) {
-  *pp = (*pp + si) % 80;
+	*pp = (*pp + si) % 80;
 }
 
 Status CreateHash(HashTable *H, char *filename) {
-  ElemType me;
-  char res[400];
-  char *dilm = " \n";
-  int id = 1;
-  H->count = 0;
-  FILE *stream;
-  if((stream = fopen(filename,"r")) == NULL)
-    {
-      fprintf(stderr,"Can not open output file.\n");
-      return 0;
-    }
-  res[fread(res, 1, 400, stream)] = 0;
-  fclose(stream);
-  for (int i = 0; i < 80; i++) {
-    H->elem[i].key[0] = 0;
-  }
-  char *k = strtok(res, dilm);
-  strcpy(me.key, k);
-  me.val = id;
-  InsertHash(H, me);
-  while ((k = strtok(NULL, dilm)) != NULL) {
-    strcpy(me.key, k);
-    me.val = ++id;
-    InsertHash(H, me);
-  }
+	ElemType me;
+	char res[400];
+	char *dilm = " \n";
+	int id = 1;
+	H->count = 0;
+	FILE *stream;
+	if ((stream = fopen(filename, "r")) == NULL) {
+		fprintf(stderr, "Can not open output file.\n");
+		return 0;
+	}
+	res[fread(res, 1, 400, stream)] = 0;
+	fclose(stream);
+	for (int i = 0; i < 80; i++) {
+		H->elem[i].key[0] = 0;
+	}
+	char *k = strtok(res, dilm);
+	strcpy(me.key, k);
+	me.val = id;
+	InsertHash(H, me);
+	while ((k = strtok(NULL, dilm)) != NULL) {
+		strcpy(me.key, k);
+		me.val = ++id;
+		InsertHash(H, me);
+	}
 
-  return OK;
+	return OK;
 }
 
 int main() {
-  char *filename = "Src";
-  HashTable H;
-  H.elem = malloc(80 * sizeof(ElemType));
-  CreateHash(&H, filename);
-  for (int i = 0; i < 80; i++) {
-    if(H.elem[i].key[0] != 0)
-      printf("%d\t%s\n", H.elem[i].val, H.elem[i].key);
-  }
-  FILE *stream;
-  if((stream = fopen("test","r")) == NULL)
-    {
-      fprintf(stderr,"Can not open output file.\n");
-      return 0;
-    }
+	char *filename = "Src";
+	HashTable H;
+	H.elem = malloc(80 * sizeof(ElemType));
+	CreateHash(&H, filename);
+	for (int i = 0; i < 80; i++) {
+		if (H.elem[i].key[0] != 0)
+			printf("%d\t%s\n", H.elem[i].val, H.elem[i].key);
+	}
+	FILE *stream;
+	if ((stream = fopen("test", "r")) == NULL) {
+		fprintf(stderr, "Can not open output file.\n");
+		return 0;
+	}
 
-  char res[50] = {0};
-  char *st = res, *fi = res;
-  char word[25];
-  int i = 0, truncate = 0, j = 0;
-
-  while(j = preProc(stream, st)){
-    if(truncate == 1){
-      while(isalnum(*fi) && fi <= st + j){
-	word[i ++] = *fi;
-	fi ++;
-      }
-      int a = 0;
-      if(SearchHash(H, word, &a)){
-	printf("Find!\n");
-      }
-      else
-	printf("NoResults!\n");
-    }
-    for(; fi <= st + j; ){
-      if(isalpha(*fi)){
+	char res[50] = { 0 };
+	char *st = res, *fi = res;
 	char word[25];
-	i = 0;
-	while(isalnum(*fi) && fi <= st + j){
-	  word[i ++] = *fi;
-	  fi ++;
+	int i = 0, truncate = 0, j = 0;
+
+	while (j = preProc(stream, st)) {
+		if (truncate == 1) {
+			while (isalnum(*fi) && fi <= st + j) {
+				word[i++] = *fi;
+				fi++;
+			}
+			int a = 0;
+			if (SearchHash(H, word, &a)) {
+				printf("Find!\n");
+			} else
+				printf("NoResults!\n");
+		}
+		for (; fi <= st + j;) {
+			if (isalpha(*fi)) {
+				char word[25];
+				i = 0;
+				while (isalnum(*fi) && fi <= st + j) {
+					word[i++] = *fi;
+					fi++;
+				}
+				if (fi == st + 25) {
+					truncate = 1;
+					break;
+				}
+				int a;
+				word[i] = 0;
+				if (SearchHash(H, word, &a)) {
+					printf("Find!\n");
+				} else
+					printf("NoResults!\n");
+			} else {
+				fi++;
+				continue;
+			}
+
+		}
+		if (res == st)
+			st += 25;
+		else {
+			st = res;
+			fi = res;
+		}
 	}
-	if(fi == st + 25){
-	  truncate = 1;
-	  break;
-	}
-	int a;
-	word[i] = 0;
-	if(SearchHash(H, word, &a)){
-	  printf("Find!\n");
-	}
-	else
-	  printf("NoResults!\n");
-      }
-      else{
-	fi ++;
-	continue;
-      }
- 
-    }
-    if(res == st)
-      st += 25;
-    else{
-      st = res;
-      fi = res;
-    }
-  }
-  return 0;
+	return 0;
 }
--- preProc.c
+++ preProc.c
@@ -3,41 +3,40 @@
 #include <ctype.h>
 #define MAX_BUF_LEN 25
 
-int preProc(FILE *stream, char *res){
-  int ch, j = 0;
-  for(int i = 0; i < MAX_BUF_LEN && (ch = fgetc(stream)) != EOF; ){
-    if(ch == '/'){
-      ch = fgetc(stream);
-      if(ch != '*'){
-	ungetc(ch, stream);
-      }
-      if(ch == '*'){
-	while(fgetc(stream) != '*' || fgetc(stream) != '/')
-	  ;
-	continue;
-      }
-    }
-    if(!isspace(ch)){
-      res[i ++] = ch;
-      j ++;
-    }
-    else {
-      while((ch = fgetc(stream)) != EOF){
-	if(isspace(ch)){
-	  continue;
-	}
-	break;
-      }
-      if(ch == EOF)
-	break;
-      ungetc(ch, stream);
-      if(i == 0)
-	continue;
-      res[i] = ' ';
-      i ++;
-      j ++;
-    }
+int preProc(FILE *stream, char *res) {
+	int ch, j = 0;
+	for (int i = 0; i < MAX_BUF_LEN && (ch = fgetc(stream)) != EOF;) {
+		if (ch == '/') {
+			ch = fgetc(stream);
+			if (ch != '*') {
+				ungetc(ch, stream);
+			}
+			if (ch == '*') {
+				while (fgetc(stream) != '*' || fgetc(stream) != '/')
+					;
+				continue;
+			}
+		}
+		if (!isspace(ch)) {
+			res[i++] = ch;
+			j++;
+		} else {
+			while ((ch = fgetc(stream)) != EOF) {
+				if (isspace(ch)) {
+					continue;
+				}
+				break;
+			}
+			if (ch == EOF)
+				break;
+			ungetc(ch, stream);
+			if (i == 0)
+				continue;
+			res[i] = ' ';
+			i++;
+			j++;
+		}
 
-  }
-  return j;
+	}
+	return j;
 }
